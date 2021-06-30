bae56b8e105dda5f2d6acf41ca3110889cd3f021
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-06-30 10:40:52 +0300

    lab37 done

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+export default function CookieStandHeader() {
+    return (
+        <header className="p-5 text-3xl text-left bg-green-500 font h-15">
+            <h1>
+                Cookie Stand Admin
+            </h1>
+        
+        </header>
+    )
+}
